121352a1538dc17cc8f98be0bfcf61bef54816c8
sanku <sankukei@gmail.com>
2024-11-22 21:29:24 +0100

    add x and p

--- ft_printf.c
+++ ft_printf.c
@@ -2,7 +2,9 @@
 #include <unistd.h>
 #include <stdio.h>
 #include "ft_printf.h"
+#include <string.h>
 
+int	ft_print_hexa(int *arr);
 int	ft_printf(const char *format, ...)
 {
 	va_list	args;
@@ -61,6 +63,60 @@ int	ft_putnbr(int n)
 	return (0);
 }
 
+int	ft_chackal(unsigned long n)
+{
+	int	i;
+
+	i = 0;
+	while (n > 16)
+	{
+		n /= 16;
+		i++;
+	}
+	return (i + 1);
+}
+
+int	ft_convert_hexa(void *ptr, char *set)
+{
+	unsigned long res;
+	int	count;
+	int	sinistre[250];
+	unsigned long	len;
+	
+	res = 0;
+	res = (unsigned long)ptr;
+	len = ft_chackal(res);
+	sinistre[len] = '\0';
+	while (len--)
+	{
+		sinistre[len] = res % 16;
+		sinistre[len] = set[sinistre[len]];
+		res /= 16;
+	}
+	count = ft_print_hexa(sinistre);
+	return (count);
+}
+
+int	ft_print_hexa(int *arr)
+{
+	int	i;
+
+	i = 0;
+	while(arr[i])
+		write(1, &arr[i++], 1);
+	return (i);
+}
+
+int	ft_print_ptr(void *ptr)
+{
+	int	count;
+
+	count = 0;
+	count += write(1, "0x", 2);
+	count += ft_convert_hexa(ptr, "0123456789abcdefg");
+	return (count);
+}
+
 int	ft_args(char c, va_list args)
 {
 	if (c == 'c')
@@ -71,13 +127,24 @@ int	ft_args(char c, va_list args)
 		return (ft_putnbr(va_arg(args, int)));
 	if (c == 'i')
 		return (ft_putnbr(va_arg(args, int)));
+	if (c == 'x')
+		return (ft_convert_hexa(va_arg(args, void *), "0123456789abcdefg"));
+	if (c == 'p')
+		return (ft_print_ptr(va_arg(args, void *)));
 	return (0);
 }
 int main(void)
 {
+	void	*ptr = "azea";
 	ft_printf("%c %c %c", 'a','b','c');
 	printf("\n");
 	ft_printf("%s", "Bonjour a tous les amis, ceci est une string de mon printf");
 	printf("\n");
 	ft_printf("%d %s", 667, " ekip");
+	printf("\n");
+	ft_printf("%x", 1234);
+	printf("\n");
+	ft_printf("%p", ptr);
+	printf("\n");
+	printf("%p", ptr);
 }
